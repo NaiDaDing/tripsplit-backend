2c52a4d48898d10ff56fa77781840e4379a82adb
NaiDa Ding <qq122618071@gmail.com>
2026-06-28 23:27:17 +0800

    feat: add expense API

--- expenses/models.py
+++ expenses/models.py
@@ -1,3 +1,26 @@
+import uuid
+
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from trips.models import Trip
+
+
+class Expense(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE, related_name='expenses')
+    title = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    paid_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='paid_expenses',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.generics import ListCreateAPIView
 
-# Create your views here.
+from expenses.models import Expense
+from expenses.serializers import ExpenseSerializer
+from trips.models import Trip
+
+
+class TripExpenseListCreateView(ListCreateAPIView):
+    serializer_class = ExpenseSerializer
+
+    def get_trip(self):
+        return get_object_or_404(
+            Trip,
+            id=self.kwargs['trip_id'],
+            created_by=self.request.user,
+        )
+
+    def get_queryset(self):
+        return Expense.objects.filter(trip=self.get_trip())
+
+    def perform_create(self, serializer):
+        serializer.save(trip=self.get_trip(), paid_by=self.request.user)
